e787833008147149cc2a7fd2e5886a9e8a5d5383
YouRa <allakhverdovyuriy@gmail.com>
2020-05-06 18:46:39 +0300

    Add QuizFragment

---
+++ app/src/main/java/com/android/uraall/soccerquiz/QuizItem.kt
@@ -0,0 +1,13 @@
+package com.android.uraall.soccerquiz
+
+class QuizItem {
+
+    var question: String
+    var answer: List<String>
+
+    constructor(question: String, answer: List<String>) {
+        this.question = question
+        this.answer = answer
+    }
+
+}
--- app/src/main/java/com/android/uraall/soccerquiz/WelcomeScreenFragment.kt
+++ app/src/main/java/com/android/uraall/soccerquiz/WelcomeScreenFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.android.uraall.soccerquiz.databinding.FragmentWelcomeScreenBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +18,12 @@ class WelcomeScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome_screen, container, false)
+        val binding: FragmentWelcomeScreenBinding =
+            DataBindingUtil.inflate(
+                inflater, R.layout.fragment_welcome_screen, container, false
+            )
+
+        return binding.root
     }
 
 }
