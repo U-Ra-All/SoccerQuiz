9692ca5fa6afb8db87f5d1894b81f9f78beb1a00
YouRa <allakhverdovyuriy@gmail.com>
2020-05-07 09:08:13 +0300

    Add Navigation

--- app/src/main/java/com/android/uraall/soccerquiz/WelcomeScreenFragment.kt
+++ app/src/main/java/com/android/uraall/soccerquiz/WelcomeScreenFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.android.uraall.soccerquiz.databinding.FragmentWelcomeScreenBinding
 
 /**
@@ -22,6 +23,10 @@ class WelcomeScreenFragment : Fragment() {
             DataBindingUtil.inflate(
                 inflater, R.layout.fragment_welcome_screen, container, false
             )
+
+        binding.letsPlayButton.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_welcomeScreenFragment_to_quizFragment)
+        }
 
         return binding.root
     }
