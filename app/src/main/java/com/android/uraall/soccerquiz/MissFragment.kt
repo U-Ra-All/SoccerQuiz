ca96b4f1008f5d2bb794cbe592d0bcb5c36466c2
YouRa <allakhverdovyuriy@gmail.com>
2020-05-07 12:39:54 +0300

    PopUpActions

--- app/src/main/java/com/android/uraall/soccerquiz/MissFragment.kt
+++ app/src/main/java/com/android/uraall/soccerquiz/MissFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.android.uraall.soccerquiz.databinding.FragmentGoalBinding
+import com.android.uraall.soccerquiz.databinding.FragmentMissBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +20,17 @@ class MissFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_miss, container, false)
+        val binding = DataBindingUtil.inflate<FragmentMissBinding>(
+            inflater, R.layout.fragment_miss, container, false
+        )
+
+        binding.oneMoreTimeButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                R.id.action_missFragment_to_quizFragment
+            )
+        }
+
+        return binding.root
     }
 
 }
