ab6f8723e3be9a430a7d39012b6932f71cd34f08
YouRa <allakhverdovyuriy@gmail.com>
2020-05-08 12:07:22 +0300

    Add NavDrawer

--- app/src/main/java/com/android/uraall/soccerquiz/MainActivity.kt
+++ app/src/main/java/com/android/uraall/soccerquiz/MainActivity.kt
@@ -3,11 +3,13 @@ package com.android.uraall.soccerquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.android.uraall.soccerquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +18,16 @@ class MainActivity : AppCompatActivity() {
             this, R.layout.activity_main
         )
 
+        drawerLayout = binding.drawerLayout
+
         val navController = this.findNavController(R.id.appNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController,
+            drawerLayout)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.appNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
